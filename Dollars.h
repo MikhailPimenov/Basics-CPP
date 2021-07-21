6a5b4adcadf8dcd43cccb31704e4be05fcea94f6
MikhailPimenov <misha4git@yandex.ru>
2021-07-21 22:13:26 +0300

    overloading-operators-using-normal-functions

--- Dollars.h
+++ Dollars.h
@@ -8,16 +8,10 @@ private:
 
 public:
 	Dollars(int dollars);
-	Dollars(const Dollars &object);
-	Dollars(Dollars &&object);
-
-	Dollars& operator=(const Dollars& object);
-	Dollars& operator=(Dollars&& object) noexcept;
-
-
-	friend Dollars operator+(const Dollars& object1, const Dollars& object2);
-	friend Dollars operator+(const Dollars& object, int dollars);
-	friend Dollars operator+(int dollars, const Dollars& object);
 
 	int getDollars() const;
 };
+
+Dollars operator+(const Dollars& object1, const Dollars& object2);
+Dollars operator+(const Dollars& object, int dollars);
+Dollars operator+(int dollars, const Dollars& object);
