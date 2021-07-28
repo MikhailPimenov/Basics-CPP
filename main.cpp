78fd8af1a0c2b4a8507391524f1e3da7582ff66d
MikhailPimenov <misha4git@yandex.ru>
2021-07-28 22:31:49 +0300

    std-move

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <utility>
+#include <vector>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T temp(a);
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+void swap_move(T& a, T& b) {
+	T temp(std::move(a));
+	a = std::move(b);
+	b = std::move(temp);
+}
+
 
 int main() {
-	std::cout << "Hello, world!\n";
+	std::string s1("John");
+	std::string s2("Andrew");
+
+    std::cout << "s1: " << s1 << '\n';
+    std::cout << "s2: " << s2 << '\n';
+	std::cout << '\n';
+
+	swap(s1, s2);
+
+    std::cout << "s1: " << s1 << '\n';
+    std::cout << "s2: " << s2 << '\n';
+	std::cout << '\n';
+
+	swap_move(s1, s2);
+
+	std::cout << "s1: " << s1 << '\n';
+	std::cout << "s2: " << s2 << '\n';
+	std::cout << '\n';
+
+
+	std::vector<std::string> vector;
+	vector.push_back(s1);
+	std::cout << "s1: " << s1 << '\n';
+	vector.push_back(std::move(s2));
+	std::cout << "s2 after it has been moved: " << s2 << '\n';
 
 	return 0;
 }
