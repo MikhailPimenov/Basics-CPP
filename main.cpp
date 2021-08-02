0856034b146052861a2f22d2edf47646f7bd92c5
MikhailPimenov <misha4git@yandex.ru>
2021-08-02 22:24:35 +0300

    template pack without universal references but with overloading

--- main.cpp
+++ main.cpp
@@ -2,47 +2,62 @@
 #include <utility>
 #include <vector>
 
-template <typename T>
-void swap(T& a, T& b) {
-	T temp(a);
-	a = b;
-	b = temp;
-}
-
-template <typename T>
-void swap_move(T& a, T& b) {
-	T temp(std::move(a));
-	a = std::move(b);
-	b = std::move(temp);
-}
+class Verbose_string final: public std::string {
+	using Base = std::string;
+public:
+	template<typename... Args>
+	Verbose_string(Args... args) : 
+		Base(std::forward<Args>(args)...) {
+		std::cout << "Verbose_string(Args...):\n";
+	}
+	Verbose_string(const std::string& string) :
+		Base(string) {
+		std::cout << "Verbose_string(const std::string&):\n";
+	}
+	Verbose_string(std::string&& string) :
+		Base(std::move_if_noexcept(string)) {
+		std::cout << "Verbose_string(std::string&&):\n";
+	}
+	Verbose_string(const Verbose_string& object) :
+		Base(static_cast<const Base&>(object)) {
+		std::cout << "Verbose_string(const Verbose_string&):\n";
+	}
+	Verbose_string(Verbose_string&& object) noexcept:
+		Base(std::move_if_noexcept(static_cast<Base&>(object))) {
+		std::cout << "Verbose_string(Verbose_string&&):\n";
+	}
+	
+};
 
 
 int main() {
-	std::string s1("John");
-	std::string s2("Andrew");
+	std::string source("Hello, world!");
 
-    std::cout << "s1: " << s1 << '\n';
-    std::cout << "s2: " << s2 << '\n';
-	std::cout << '\n';
-
-	swap(s1, s2);
-
-    std::cout << "s1: " << s1 << '\n';
-    std::cout << "s2: " << s2 << '\n';
-	std::cout << '\n';
-
-	swap_move(s1, s2);
-
-	std::cout << "s1: " << s1 << '\n';
-	std::cout << "s2: " << s2 << '\n';
-	std::cout << '\n';
+	std::cout << "Expected Verbose_string(std::string&&):\n";
+	Verbose_string string(std::string("Hello, world!"));
+	std::cout << string << '\n';
 
 
-	std::vector<std::string> vector;
-	vector.push_back(s1);
-	std::cout << "s1: " << s1 << '\n';
-	vector.push_back(std::move(s2));
-	std::cout << "s2 after it has been moved: " << s2 << '\n';
+	std::cout << "Expected Verbose_string(const Verbose_string&):\n";
+	Verbose_string string2(string);
+	std::cout << string2 << '\n';
+
+
+	std::cout << "Expected Verbose_string(Args...):\n";
+	Verbose_string string3(source.rbegin(), source.rend());
+	std::cout << string3 << '\n';
+
+
+	std::cout << "If no_except-rule is upheld, expected Verbose_string(Verbose_string&&):\n";
+	Verbose_string string4(std::move_if_noexcept(string3));
+	std::cout << string4 << '\n';
+	std::cout << string3 << '\n';
+
+
+	std::cout << "If no_except-rule is upheld, expected Verbose_string(std::string&&):\n";
+	Verbose_string string5(std::move_if_noexcept(source));
+	std::cout << string5 << '\n';
+	std::cout << source << '\n';
 
 	return 0;
 }
